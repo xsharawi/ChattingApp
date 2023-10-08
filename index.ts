57c7a92cc01676812e623b4f2a968a0551d8f299
Mohammad-AlTmimi <mohammaedtamimei@gmail.com>
2023-10-08 18:50:59 +0300

    route

--- index.ts
+++ index.ts
@@ -3,11 +3,21 @@ import os from 'os'
 import 'dotenv/config'
 import * as db from "./DB/dataSource.js"
 
+import userRoute from './routes/users.js'
+import contactRoute from './routes/users.js'
+import groupRoute from './routes/users.js'
+import chatRoute from './routes/users.js'
+
+
 const PORT = process.env.PORT
 const HOST = os.hostname();
 
 const app = express()
 
+app.use('/users' , userRoute);
+app.use('/chats' , contactRoute);
+app.use('/groups' , groupRoute);
+app.use('/contacts' , contactRoute);
 
 
 

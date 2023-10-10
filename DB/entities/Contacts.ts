7d36b2b79706fe6d42b7f202c415005e3e5a3b3b
Mohammad-AlTmimi <mohammaedtamimei@gmail.com>
2023-10-10 21:53:19 +0300

    Contact DB

--- DB/entities/Contacts.ts
+++ DB/entities/Contacts.ts
@@ -2,25 +2,6 @@ import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, CreateDateColumn } 
 
 @Entity()
 export class Contact extends BaseEntity{
-    @PrimaryGeneratedColumn('uuid')
-    chat_id: string
-
     @Column()
-    sender_id:string
-
-    @Column()
-    receiver_id:string
-
-    @Column()
-    text: string
-
-    @Column()
-    encryption_key: string
-
-    @CreateDateColumn({
-        type: 'timestamp',
-        default: () => "CURRENT_TIMESTAMP()"
-    })
-    sent_at: Date;
-
+    contact_id: string
 }

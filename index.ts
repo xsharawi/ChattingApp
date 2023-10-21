e367897d878760224fbf4bc7dfa9a9fa30400774
Mohammad-AlTmimi <mohammaedtamimei@gmail.com>
2023-10-21 22:10:38 +0300

    Starting with socket :( :(

--- index.ts
+++ index.ts
@@ -1,27 +1,40 @@
-import express from 'express'
-import os from 'os'
-import 'dotenv/config'
-import * as db from "./DB/dataSource.js"
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+import os from 'os';
+import 'dotenv/config';
+import * as db from "./DB/dataSource.js";
 
-import userRoute from './routes/users.js'
-import contactRoute from './routes/users.js'
-import groupRoute from './routes/users.js'
-import chatRoute from './routes/users.js'
+import userRoute from './routes/users.js';
+import contactRoute from './routes/users.js';
+import groupRoute from './routes/users.js';
+import chatRoute from './routes/users.js';
 
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+const connectedClients = new Map<string, WebSocket>();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const HOST = os.hostname();
 
-const app = express()
+app.use('/users', userRoute);
+app.use('/chats', contactRoute);
+app.use('/groups', groupRoute);
+app.use('/contacts', contactRoute);
 
-app.use('/users' , userRoute);
-app.use('/chats' , contactRoute);
-app.use('/groups' , groupRoute);
-app.use('/contacts' , contactRoute);
+app.use(express.json());
 
+wss.on('connection', (ws, req) => {
+  console.log('WebSocket client connected.');
+  
 
+  ws.on('message', (message) => {
+    console.log(`Received from : ${message}`);
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log(`SERVER RUNNING ON http://${HOST}:${PORT}`)
-    db.init()
-})
+app.listen(PORT, () => {
+  console.log(`SERVER RUNNING ON http://${HOST}:${PORT}`);
+  db.init();
+});

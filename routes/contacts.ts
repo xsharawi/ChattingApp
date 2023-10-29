eae2a6258e6faa4bc3f572caf1703c2ab4ce7042
Mohammad-AlTmimi <mohammaedtamimei@gmail.com>
2023-10-30 00:06:16 +0300

    build contact

--- routes/contacts.ts
+++ routes/contacts.ts
@@ -38,5 +38,110 @@ router.post('/add' , authenticate , async (req , res , next) => {
   }
 })
 
+router.delete('/delete' , authenticate , async (req , res , next) =>{
+  try{
+      const userId = res.locals.user.id;
+      const { user2 } = req.body
+      const user = await User.findOneBy({ id : userId });
+      if(!user)
+        next({error : `no user in contact/delete`});
+      if(user){
+        const cont = user.contacts.filter((id) => id !== user2);
+        const block = user.blockcontact.filter((id) => id !== user2);
+        const mute = user.mutecontact.filter((id) => id !== user2);
+        user.contacts = cont;  
+        user.blockcontact = block;
+        user.mutecontact = mute;
+        await user.save()
+        res.status(200).send("its deleted")
+      }
+  }catch(err){
+    next({ error : err });
+  }
+})
 
+router.put('/block' , authenticate , async(req , res , next) =>{
+  try{
+      const userId = res.locals.user.id;
+      const {user2} = req.body;
+      const user = await User.findOneBy({ id : userId });
+      const blockUser = await User.findOneBy({ id : user2 });
+      if(!user || !blockUser){
+        next({error: `user sender or user Block is not find from contact/block`});
+      }
+      else{
+        const cont = user.contacts.filter((id) => id !== user2);
+        user.contacts = cont;
+        user.blockcontact.push(user2);
+        await user.save();
+        res.status(200).send(`user blocked `);
+      }
+  }catch(err){
+    next({ error : err })
+  }
+})
+
+router.put('/mute' , authenticate , async(req , res , next) =>{
+  try{
+      const userId = res.locals.user.id;
+      const {user2} = req.body;
+      const user = await User.findOneBy({ id : userId });
+      const muteUser = await User.findOneBy({ id : user2 });
+      if(!user || !muteUser){
+        next({error: `user sender or user Mute is not find from contact/mute`});
+      }
+      else{
+        const cont = user.contacts.filter((id) => id !== user2);
+        user.contacts = cont;
+        user.mutecontact.push(user2);
+        await user.save();
+        res.status(200).send(`user Mute `);
+      }
+  }catch(err){
+    next({ error : err })
+  }
+})
+
+router.put('/unblock' , authenticate , async(req , res , next) =>{
+  try{
+      const userId = res.locals.user.id;
+      const {user2} = req.body;
+      const user = await User.findOneBy({ id : userId });
+      const blockUser = await User.findOneBy({ id : user2 });
+      if(!user || !blockUser){
+        next({error: `user sender or user Block is not find from contact/block`});
+      }
+      else{
+        const block = user.blockcontact.filter((id) => id !== user2);
+        user.blockcontact = block;
+        user.contacts.push(user2);
+
+        await user.save();
+        res.status(200).send(`user unBlocked `);
+      }
+  }catch(err){
+    next({ error : err })
+  }
+})
+
+router.put('/unmute' , authenticate , async(req , res , next) =>{
+  try{
+      const userId = res.locals.user.id;
+      const {user2} = req.body;
+      const user = await User.findOneBy({ id : userId });
+      const muteUser = await User.findOneBy({ id : user2 });
+      if(!user || !muteUser){
+        next({error: `user sender or user Mute is not find from contact/mute`});
+      }
+      else{
+        const mute = user.mutecontact.filter((id) => id !== user2);
+        user.mutecontact = mute;
+        user.contacts.push(user2);
+        await user.save();
+        res.status(200).send(`user unMuted `);
+      }
+  }catch(err){
+    next({ error : err })
+  }
+})
 export default router;
